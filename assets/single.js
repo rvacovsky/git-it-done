7c19b1f8b5ae4777b923760266ed3801a86cf467
Roger Vacovsky <rvacovsky@gmail.com>
2022-04-08 17:48:14 -0400

    added error handling, path between two pages

--- assets/single.js
+++ assets/single.js
@@ -2,6 +2,22 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -18,8 +34,7 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
+    } else {
       console.log(response);
       alert("There was a problem with your request!");
     }
@@ -44,7 +59,6 @@ var displayIssues = function(issues) {
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
     
-  
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -54,8 +68,7 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
@@ -81,4 +94,5 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues();
+
